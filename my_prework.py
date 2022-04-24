8cb4255a30cbea9c914362db39e82e53de1516d3
Chris Oviedo <coviedo911@gmail.com>
2022-04-24 00:12:48 -0600

    first commit

---
+++ my_prework.py
@@ -0,0 +1,15 @@
+# Question 1:
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function
+
+def hello_name(user_name):
+    # your code here 
+    pass
+
+# Question 2 
+# write a pythin function, firdt_odds that prints the odd numbers from 1-100 and returns nothing 
+def first_odds():
+    #your code here
+    pass
+
+# ect ect ect...
+
